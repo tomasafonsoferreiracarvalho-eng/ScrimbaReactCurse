c30d605d524f28b852745a35c45b4da76cdca745
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2026-07-06 17:24:16 +0100

    1 commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {createRoot} from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"))
+
+root.render(<ul>
+                <li>É uma das bibliotecas mais populares e requisitadas do mercado.</li>
+                <li>A arquitetura baseada em componentes torna o código reutilizável.</li>
+                <li>O ecossistema é gigante e facilita a criação de apps web e mobile.</li>
+            </ul>)
