0cde8a2ed4e4e42445681080b761cf385b8ea2f1
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2026-07-07 19:18:39 +0100

    Refactor main.jsx to create Ulist component and improve structure of MainTest and Mybutton

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,37 @@ import {createRoot} from "react-dom/client"
 
 const root = createRoot(document.querySelector(".root"))
 
+function Ulist() {
+    return (
+        <ul>
+            <li>É uma das bibliotecas mais populares e requisitadas do mercado.</li>
+            <li>A arquitetura baseada em componentes torna o código reutilizável.</li>               
+            <li>O ecossistema é gigante e facilita a criação de apps web e mobile.</li>
+        </ul>
+    )
+}
 function MainTest() {
     return (
-        <><h2>Este é um teste no vs code</h2>
-        <p>isto é um paragrafo</p></>
+        <section>
+            <h2>Este é um teste no vs code</h2>
+            <p>isto é um paragrafo</p>
+        </section>
+    )
+}
+function Mybutton() {
+    return (
+        <section>
+            <button>Este é um botão</button>
+        </section> 
     )
 }
 
-root.render(<><ul>
-                <li>É uma das bibliotecas mais populares e requisitadas do mercado.</li>
-                <li>A arquitetura baseada em componentes torna o código reutilizável.</li>
-                <li>O ecossistema é gigante e facilita a criação de apps web e mobile.</li>
-            </ul>
-            <MainTest /></>
-        )
+root.render(
+        <>
+            <Ulist />
+            <MainTest />
+            <Mybutton />
+        </>
+)
 
 
