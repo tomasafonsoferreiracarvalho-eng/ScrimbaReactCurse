c3f0bc04e1e21c511e0b31f9d4110a18b722ef71
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2026-07-07 18:17:15 +0100

    Add MainTest component to render additional content

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,19 @@ import {createRoot} from "react-dom/client"
 
 const root = createRoot(document.querySelector(".root"))
 
-root.render(<ul>
+function MainTest() {
+    return (
+        <><h2>Este é um teste no vs code</h2>
+        <p>isto é um paragrafo</p></>
+    )
+}
+
+root.render(<><ul>
                 <li>É uma das bibliotecas mais populares e requisitadas do mercado.</li>
                 <li>A arquitetura baseada em componentes torna o código reutilizável.</li>
                 <li>O ecossistema é gigante e facilita a criação de apps web e mobile.</li>
-            </ul>)
+            </ul>
+            <MainTest /></>
+        )
+
+
